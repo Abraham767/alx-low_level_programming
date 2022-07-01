ddfd01f76272e2fc443d6cf0ae34843936e41e45
popoola abraham <Popoolaabraham7@gmail.com>
2022-07-01 07:42:55 -0700

    Alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -11,8 +11,10 @@ int main(void)
 		char low;
 
 		for (low = 'a'; low <= 'z'; low++)
-		        putchar(low);
-			printf("\n");
+			putchar(low);
+		for (low = 'A'; low <= 'Z'; low++)
+			putchar(low);
+			putchar('\n');
 
 		return (0);
 }
