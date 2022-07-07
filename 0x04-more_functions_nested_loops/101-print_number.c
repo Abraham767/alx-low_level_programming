3bb9385b90bd53b87344018a014de50c5d0bae80
popoola abraham <Popoolaabraham7@gmail.com>
2022-07-07 15:57:36 -0700

    Prints numbers

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_number - prints an integer
- * @n: input integer
+ * print_number - a function that prints an integer
+ * @n: print int
  * Return: inputted integer
  */
 void print_number(int n)
