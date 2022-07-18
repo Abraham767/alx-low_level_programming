e2c817c5afa9d93d1e5cae1365c7a739732c00bc
popoola abraham <Popoolaabraham7@gmail.com>
2022-07-18 16:06:12 -0700

    Strspn

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ *  _strchr - locates a character in a string
+ *   @s: String
+ *   @c: Character to search
+ *   Return: Pointer to the first occurence of the character c in the string d
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0') /*declaring WHILE*/
+	{
+		if (*s == c ) /*if s == c*/
+		{
+			return (s); /*return s*/
+		}
+	++s;
+	}
+	if (*s == c)
+	{
+		return (s);
+	}
+	return (0);/* values null*/
+
+}	
