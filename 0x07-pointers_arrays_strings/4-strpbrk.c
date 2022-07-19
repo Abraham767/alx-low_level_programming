be86c004a7f53f0b12c3fed829c11230285a5352
popoola abraham <Popoolaabraham7@gmail.com>
2022-07-18 19:16:57 -0700

    Strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -8,19 +8,19 @@
 char *_strpbrk(char *s, char *accept)
 {
 	int j;
-	while (*s != '\0') /* Declaring WHILE */
-	{
-		j = 0;
-	while (accept[j] != '\0') /*Evaluating *accept*/
-	{
-		if (&s == accept[j])
+		while (*s != '\0') /* Declaring WHILE */
 		{
-			return (s);
-		}
+			j = 0;
+		while (accept[j] != '\0') /*Evaluating *accept*/
+		{
+			if (&s == accept[j])
+			{
+				return (s);
+			}
 			j++; /*add j+1*/
-	}
-	s++; /*add s+1*/
-	}
+		}
+		s++; /*add s+1*/
+		}
 	return (0);
 
 }
