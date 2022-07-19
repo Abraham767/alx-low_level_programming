0b117888a4f5767fbee571979432c9193ca495ab
popoola abraham <Popoolaabraham7@gmail.com>
2022-07-18 19:28:47 -0700

    Chessboard

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -8,10 +8,10 @@ void print_chessboard(char (*a)[8])
 {
 	/*Declaring variables*/
 	int i, j;
-	i = 0;
-	while (i < 8) /* number reptitions*/
-	{
-		j = 0;
+		i = 0;
+		while (i < 8) /* number reptitions*/
+		{
+			j = 0;
 		while (j < 8)
 		{
 			_putchar (a[i][j]);
