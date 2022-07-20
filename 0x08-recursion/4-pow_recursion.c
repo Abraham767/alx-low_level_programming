6396f40d6abbafaaf698497a1a7c6a4174d22429
popoola abraham <Popoolaabraham7@gmail.com>
2022-07-20 14:19:35 -0700

    pow

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  _pow_recursion - a function that returns the value of x
+ *  raised to tje power of y
+ *  @x: first input
+ *  @y: second input
+ *  Return: Always 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
