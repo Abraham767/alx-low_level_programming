ec9e101419655df837b3edafbdb35f0a3c4095e7
popoola abraham <Popoolaabraham7@gmail.com>
2022-07-20 14:29:36 -0700

    Sqrt

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+int _sqrt(int, int);
+/**
+ *  _sqrt_recursion - a function that return the natural
+ *  square root of a number
+ *  @n: input
+ *  Return: Always 0
+ */
+int _sqrt_recursion(int n)
+{
+	return (_sqrt(n, 1));
+}
+/**
+ *  _sqrt - checks if perfect square
+ *  @n: input
+ *  @i: counter
+ *  Return: Always 0
+ */
+int _sqrt(int n, int i)
+{
+	int square = i * i;
+
+	if (square > n)
+		return (-1);
+	if (square == n)
+		return (i);
+	return (_sqrt(n, i + 1));
+}
