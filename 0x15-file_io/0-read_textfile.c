470020fc656f72d201f7c83c285fd4c4f026f63b
popoola abraham <Popoolaabraham7@gmail.com>
2022-08-22 16:40:33 -0700

    Read

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ *  read_textfile - a function that read text file
+ *  @filename: filename
+ *  @letters: size
+ *  Return: 0
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int file;
+	ssize_t rd, wr;
+	char *buf;
+
+	if (!filename)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+
+	if (file == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	rd = read(file, buf, letters);
+	wr = write(STDOUT_FILENO, buf, rd);
+	if (wr == -1)
+		return (0);
+
+	close(file);
+	free(buf);
+	return (wr);
+}
